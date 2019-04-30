7f95c4a9ad768cc640da80ab98b6e5419df0127b
Navin Kumar <navin1551@gmail.com>
2019-04-30 12:49:22 -0700

    all done except for API token issue

--- server.js
+++ server.js
@@ -1,12 +1,50 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const MOVIEDEX = require("./moviedex.json");
+
+console.log(process.env.API_TOKEN);
 
 const app = express();
 
 app.use(morgan("dev"));
+app.use(cors());
+app.use(helmet());
 
-app.use((req, res) => {
-  res.send("Hello, World!");
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get("Authorization");
+  console.log("validate bearer token middleware");
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+    return res.status(401).json({ error: "Unauthorized request" });
+  }
+  next();
+});
+
+app.get("/movie", function handleGetMovies(req, res) {
+  let response = MOVIEDEX;
+
+  if (req.query.genre) {
+    response = response.filter(movie =>
+      movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+    );
+  }
+
+  if (req.query.country) {
+    response = response.filter(movie =>
+      movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+    );
+  }
+
+  if (req.query.avg_vote) {
+    response = response.filter(
+      movie => Number(movie.avg_vote) >= Number(req.query.avg_vote)
+    );
+  }
+
+  res.json(response);
 });
 
 const PORT = 8000;
